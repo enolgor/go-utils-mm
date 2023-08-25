ab00e004a165f5bd375102cbe63a347c5e08bad0
enolgor <enolgor@teleco.upv.es>
2023-08-25 17:10:00 +0200

    fix

--- examples/server.go
+++ examples/server.go
@@ -3,12 +3,12 @@ package examples
 import (
 	"net/http"
 
-	"github.com/enolgor/go-utils/ht"
+	"github.com/enolgor/go-utils/server"
 )
 
 func Server() {
-	auth := ht.BasicAuthHandler("admin", "test")
-	http.HandleFunc("/", auth.Finally(ht.Handle(ht.Html("<html><body>hello</body></html>"))))
+	auth := server.BasicAuthHandler("admin", "test")
+	http.HandleFunc("/", server.Handle(server.Get, auth, server.Html("<html><body>hello</body></html>")))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
