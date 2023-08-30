8e971004ab1efd55888dcbe41bd9d46f50547b30
enolgor <enolgor@teleco.upv.es>
2023-08-30 10:18:31 +0200

    example

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/enolgor/go-utils/examples"
-	"github.com/enolgor/go-utils/server/path"
 )
 
 type Key string
 
 func main() {
 	examples.Server(8080)
-	match, err := path.Matcher("/test/(.*)/hola/:asdf/(.*)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	valueMap := map[any]string{}
-	fmt.Println(match("/test/eneko/foo/hola/end/again", valueMap))
-	fmt.Println(valueMap)
 }
